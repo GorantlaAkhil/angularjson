8eb6d51e62de20cb28493e0a8b6ec0eb3d9f7692
Akhil_Gorantla <akhilgorantla01@gmail.com>
2024-04-10 22:48:01 -0400

    'as'

---
+++ angular-json-diff-jdd-master/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { JsonComparisonComponent } from './json-comparison/json-comparison.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
+@NgModule({
+  declarations: [AppComponent, JsonComparisonComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
